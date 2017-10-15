c8dd1152c430f89c160bf5f877b992f7dbdefd15
Timothy Smith <tsmith@saberastro.com>
2017-10-15 20:45:08 +1100

    Set correct time step and adjust filter for time difference

--- src/tools.cpp
+++ src/tools.cpp
@@ -5,18 +5,20 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
+Tools::Tools()
+{}
 
-Tools::~Tools() {}
+Tools::~Tools()
+{}
 
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
+VectorXd Tools::CalculateRMSE(const vector<VectorXd>& estimations,
+                              const vector<VectorXd>& ground_truth)
+{
     VectorXd rmse(4);
     rmse << 0, 0, 0, 0;
     size_t count = 0;
     for (int i = 0; i < estimations.size(); ++i)
     {
-        // ... your code here
         VectorXd err = estimations[i] - ground_truth[i];
         if ((estimations[i].size() != ground_truth[i].size())
             || estimations[i].size() == 0)
@@ -32,16 +34,35 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     rmse = rmse / count;
 
     //calculate the squared root
-    // ... your code here
     rmse = rmse.array().sqrt();
 
     //return the result
     return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state)
+{
+    MatrixXd Hj(3, 4);
+    //recover state parameters
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
+
+    double rp2 = px * px + py * py;
+    if (rp2 == 0)
+    {
+        std::cout << "warning: cannot evaluate jacobian at: (" << px << ", "
+                  << py << ")." << std::endl;
+        return Hj;
+    }
+    float rp = sqrt(rp2);
+
+    Hj <<
+       px / rp, py / rp, 0, 0,
+        -py / rp2, px / rp2, 0, 0,
+        py * (vx * py - vy * px) / rp / rp2, px * (vy * px - vx * py) / rp /
+                                             rp2, px / rp, py / rp;
+
+    return Hj;
 }
